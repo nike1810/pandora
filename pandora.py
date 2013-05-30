919e612740ca438d921298dad1492371c27dce09
nike1987(Linux) <nike.inet@rambler.ru>
2013-05-30 16:34:57 +0400

    searching sample song

--- pandora.py
+++ pandora.py
@@ -11,14 +11,15 @@ import json
 from crypto import Crypto
 import time
 
-PATT = re.compile(">(.*?)<")
+TITLE_PATT = re.compile(">(.*?)<")
+SAMPLE_PATT = re.compile("http\S*3")
 
 #https://oauth.vk.com/authorize?client_id=3665077&scope=audio&redirect_uri=https://oauth.vk.com/blank&display=wap&response_type=token
 
 COOKIE = 'at=wR3JtOkD5uML+hpRZts7J9nGAfao51kP9+mWXzdt95pb2iuYxCrLqMlU3pJwmIYf5POmKDqWEOoNNtwtzqvlH7A%3D%3D'
 CONFIG_URL = 'https://gist.github.com/ustasb/596f1ee96d03463fde77/raw/pandata_config.json'
-ACCESS_TOKEN = "e1f0ba832796b541db62b2d2a677bfe777319e5e9193892f27d17020c029dc6335280fe909a4cda4ed350"
-COUNT_OF_SONGS = 100
+ACCESS_TOKEN = "c0a3e51fe5e7755b11b85f95db0c09aafdde75d93842022842681a6f9e4daf0954f4efd1f79199fbe413b"
+COUNT_OF_SONGS = 1
 COUNT_OF_DUPLICATES = 3
 DOWNLOAD_FOLDER_NAME = "audio"
 LOGIN = "demidov.1810"
@@ -40,135 +41,142 @@ VK_SECRETKEY = "cwrhrpcPfkIydbWgH8xc"
 VK_USERNAME = "nike.net@mail.ru"
 VK_PASSWORD = "baron1810"
 
-def get_names(n = COUNT_OF_SONGS):
-    l = 1000
-    i = 1
-    out = []
-    count = 1
-
-    cookies = get_cookies()
-
-    while l>100:
-        if not n is None:
-            if i >= n:
-                break
-
-        resp = r.get("http://www.pandora.com/content/tracklikes?likeStartIndex=0&thumbStartIndex="+str(count)+"&webname="+LOGIN,
-                             headers = {"Cookie": cookies})
-        soup = bs.BeautifulSoup(resp.text)
-        for x in soup.findAll(attrs={"class":"infobox-body"}):
-            patt = [PATT.findall(str(x.a))[0], PATT.findall(str(x.p.a))[0]]
-            if patt not in out:
-                out.append([PATT.findall(str(x.a))[0], PATT.findall(str(x.p.a))[0]])
-            i += 1
-            
-        if not "infobox-body" in resp.text and not "no_more tracklike" in resp.text:
-            print "Something bad. Check your cookie"
-            exit()
-            
-        l = len(resp.text)
-        count += 1
-        
-    print len(out)
-    print out
-    return out
-
-def get_cookies():
-    response = r.get(CONFIG_URL)
-    cookies = json.loads(response.text)
-
-    return cookies["cookie"]
-
-def get_vktoken():
-    return ACCESS_TOKEN
+# def get_names(n = COUNT_OF_SONGS):
+#     l = 1000
+#     i = 1
+#     out = []
+#     count = 1
 #
-#     string = "https://oauth.vk.com/token?grant_type=password&client_id=%(client_id)s&client_secret=%(secret_key)s&username=%(username)s&password=%(password)s&scope=offline" %{"client_id" : VK_APPID, "secret_key" : VK_SECRETKEY, "username" : VK_USERNAME, "password" : VK_PASSWORD }
-#     print string
-#     resp = r.get(string)
-#     print "get_vktoken after"
-#     print resp.json()
-
-
-def audio_search(string):
-    accessToken = get_vktoken()
-    resp = r.get("https://api.vk.com/method/audio.search?q=%(q)s&sort=2&count=30&access_token=%(ACCESS_TOKEN)s"%{"q":string, "ACCESS_TOKEN":accessToken})
-    return resp.json()
-
-def fillObjectsInfo(objects):
-    i = 1
-    fillingObjects = {}
-
-    while not objects[i] is None :
-        if i > 10:
-            break
-            # print objects[i]
-        head = r.head(objects[i]["url"])
-        size = head.headers["content-length"]
-        duration = objects[i]["duration"]
-        bytrate = (float(size) * 8 / float(duration)) / 1024
-        if bytrate > 300:
-            fillingObjects[bytrate] = objects[i]
-        i+=1
-
-    return fillingObjects
-
-def sortObjects(objects):
-    sortedObjects = []
-    count = 0
-    copies = COUNT_OF_DUPLICATES
-    for key in sorted(objects.keys(), reverse=True):
-        if count >= copies:
-            break
-        sortedObjects.append(objects[key])
-        count+=1
-
-    return sortedObjects
-
-def download(folder=DOWNLOAD_FOLDER_NAME):
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-    for song in get_names():
-        if len(song) == 1:
-            song.append("Unknown")
-        try:
-            originalSongName = song[0]+" "+song[1]
-            get_vktoken()
-            objects = audio_search(originalSongName)["response"]
-            print objects
-            unsortedObject = fillObjectsInfo(objects)
-
-        except:
-            continue
-
-        sortedObjects = sortObjects(unsortedObject)
-        print sortedObjects
-
-
-        for obj in sortedObjects:
-            print "start to download", obj["title"] + "-"+obj["artist"]
-            try:
-                songPath = os.path.join(folder, originalSongName)
-                if not os.path.exists(songPath):
-                    os.mkdir(songPath)
-
-                urllib.urlretrieve(obj["url"], os.path.join(songPath,  obj["title"] + "-"+obj["artist"] +".mp3"))
-            except:
-                print 'Download fault'
-                continue
-            print obj["title"] + "-"+obj["artist"], "downloaded"
-
-
-            
+#     cookies = get_cookies()
+#
+#     while l>100:
+#         if not n is None:
+#             if i >= n:
+#                 break
+#
+#         resp = r.get("http://www.pandora.com/content/tracklikes?likeStartIndex=0&thumbStartIndex="+str(count)+"&webname="+LOGIN,
+#                              headers = {"Cookie": cookies})
+#         soup = bs.BeautifulSoup(resp.text)
+#         print soup
+#
+#         for x in soup.findAll(attrs={"class":"infobox-body"}):
+#             patt = [PATT.findall(str(x.a))[0], PATT.findall(str(x.p.a))[0]]
+#             if patt not in out:
+#                 out.append([PATT.findall(str(x.a))[0], PATT.findall(str(x.p.a))[0]])
+#             i += 1
+#
+#         if not "infobox-body" in resp.text and not "no_more tracklike" in resp.text:
+#             print "Something bad. Check your cookie"
+#             exit()
+#
+#         l = len(resp.text)
+#         count += 1
+#
+#     # print len(out)
+#     # print out
+#     return out
+#
+# def get_cookies():
+#     response = r.get(CONFIG_URL)
+#     cookies = json.loads(response.text)
+#
+#     return cookies["cookie"]
+#
+# def get_vktoken():
+#     return ACCESS_TOKEN
+# #
+# #     string = "https://oauth.vk.com/token?grant_type=password&client_id=%(client_id)s&client_secret=%(secret_key)s&username=%(username)s&password=%(password)s&scope=offline" %{"client_id" : VK_APPID, "secret_key" : VK_SECRETKEY, "username" : VK_USERNAME, "password" : VK_PASSWORD }
+# #     print string
+# #     resp = r.get(string)
+# #     print "get_vktoken after"
+# #     print resp.json()
+#
+#
+# def audio_search(string):
+#     accessToken = get_vktoken()
+#     resp = r.get("https://api.vk.com/method/audio.search?q=%(q)s&sort=2&count=30&access_token=%(ACCESS_TOKEN)s"%{"q":string, "ACCESS_TOKEN":accessToken})
+#     return resp.json()
+#
+# def fillObjectsInfo(objects):
+#     i = 1
+#     fillingObjects = {}
+#
+#     while not objects[i] is None :
+#         if i > 10:
+#             break
+#             # print objects[i]
+#         head = r.head(objects[i]["url"])
+#         size = head.headers["content-length"]
+#         duration = objects[i]["duration"]
+#         bytrate = (float(size) * 8 / float(duration)) / 1024
+#         if bytrate > 300:
+#             fillingObjects[bytrate] = objects[i]
+#         i+=1
+#
+#     return fillingObjects
+#
+# def sortObjects(objects):
+#     sortedObjects = []
+#     count = 0
+#     copies = COUNT_OF_DUPLICATES
+#     for key in sorted(objects.keys(), reverse=True):
+#         if count >= copies:
+#             break
+#         sortedObjects.append(objects[key])
+#         count+=1
+#
+#     return sortedObjects
+#
+# def download(folder=DOWNLOAD_FOLDER_NAME):
+#     if not os.path.exists(folder):
+#         os.mkdir(folder)
+#     for song in get_names():
+#         if len(song) == 1:
+#             song.append("Unknown")
+#         try:
+#             originalSongName = song[0]+" "+song[1]
+#             get_vktoken()
+#             objects = audio_search(originalSongName)["response"]
+#             print objects
+#             unsortedObject = fillObjectsInfo(objects)
+#
+#         except:
+#             continue
+#
+#         sortedObjects = sortObjects(unsortedObject)
+#         print sortedObjects
+#
+#
+#         for obj in sortedObjects:
+#             print "start to download", obj["title"] + "-"+obj["artist"]
+#             try:
+#                 songPath = os.path.join(folder, originalSongName)
+#                 if not os.path.exists(songPath):
+#                     os.mkdir(songPath)
+#
+#                 urllib.urlretrieve(obj["url"], os.path.join(songPath,  obj["title"] + "-"+obj["artist"] +".mp3"))
+#             except:
+#                 print 'Download fault'
+#                 continue
+#             print obj["title"] + "-"+obj["artist"], "downloaded"
+#
+#
+#
 #
 # download()
 
 
 
 # ------------------------------------------------------------------------------------------------------------------
+class PandoraSong:
+    def __init__(self):
+        self.artist = ""
+        self.title = ""
+        self.sample = ""
 class Song:
     def __init__(self):
         self.artist = ""
-        self.name  = ""
+        self.title= ""
         self.duration = 0
         self.bitrate = 0
         self.url =  ""
@@ -184,23 +192,36 @@ class Pandora:
 
     def getLikedTracks(self, countOfSongs = 100):
         l = 1000
-        i = 1
-        out = []
+        # nameCounter = 1
+        # sampleCounter = 1
+        artistNames = []
+        sampleSongs = {}
         count = 1
 
         while l>100:
             if not countOfSongs is None:
-                if i >= countOfSongs:
+                if len(artistNames) >= countOfSongs:
                     break
 
             resp = r.get("http://www.pandora.com/content/tracklikes?likeStartIndex=0&thumbStartIndex="+str(count)+"&webname="+self.login,
                                  headers = {"Cookie": self.cookie})
             soup = bs.BeautifulSoup(resp.text)
+            # print soup
             for x in soup.findAll(attrs={"class":"infobox-body"}):
-                patt = [PATT.findall(str(x.a))[0], PATT.findall(str(x.p.a))[0]]
-                if patt not in out:
-                    out.append([PATT.findall(str(x.a))[0], PATT.findall(str(x.p.a))[0]])
-                i += 1
+                patt = [TITLE_PATT.findall(str(x.a))[0], TITLE_PATT.findall(str(x.p.a))[0]]
+                if patt not in artistNames:
+                    artistNames.append([TITLE_PATT.findall(str(x.a))[0], TITLE_PATT.findall(str(x.p.a))[0]])
+                    # nameCounter += 1
+
+            for sampleStr in soup.findAll(attrs={"class" : "int-icon-1 i-play-1 sample"}):
+                sample = re.findall(SAMPLE_PATT, str(sampleStr))
+                comparePatt = re.compile("\/[^\/]*.mp3")
+                compare = re.findall(comparePatt, str(sample))
+                if compare not in sampleSongs.keys():
+                    sampleSongs[compare[0]] = sample
+                    # sampleSongs.pop(compare, sample)
+                    # sampleCounter+=1
+
 
             if not "infobox-body" in resp.text and not "no_more tracklike" in resp.text:
                 print "Something bad. Check your cookie"
@@ -209,9 +230,11 @@ class Pandora:
             l = len(resp.text)
             count += 1
 
-        print len(out)
-        print out
-        return out
+        if(len(artistNames) != len(sampleSongs)):
+            print "Mismatch count of songs: %s & artist: %s" % (len(sampleSongs), len(artistNames))
+        # print len(out)
+        print artistNames
+        return artistNames
 
 class VKcom:
     def __init__(self):
@@ -239,13 +262,13 @@ class Downloader:
         for song in self.pandora.getLikedTracks():
             if len(song) == 1:
                 song.append("Unknown")
-            # try:
-            originalSongName = song[0]+" "+song[1]
-            objects = self.vk.audioSearch(originalSongName)["response"]
-            songs = self.getSongsInfo(objects)
+            try:
+                originalSongName = song[0]+" "+song[1]
+                objects = self.vk.audioSearch(originalSongName)["response"]
+                songs = self.getSongsInfo(objects)
 
-            # except:
-            #     continue
+            except:
+                continue
 
             if self.useFilterBitrate :
                 songs = self.filterSongs(songs)
